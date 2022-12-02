c58b79ae4b35bd0734509c34dc81513dd5f23c31
Kade Robertson <kade@kaderobertson.dev>
2022-12-02 08:00:45 -0500

    feat: solution for day 2

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@ use crate::problem::Problem;
 mod macros;
 mod problem;
 mod problem_01;
+mod problem_02;
 mod util;
 
 fn main() {
     println!("~ Advent of Code 2021 ~");
 
-    let problems: Vec<Box<dyn Problem>> = vec![Box::new(problem_01::Problem01::new())];
+    let problems: Vec<Box<dyn Problem>> = vec![
+        Box::new(problem_01::Problem01::new()),
+        Box::new(problem_02::Problem02::new()),
+    ];
     let mut duration = Instant::now().elapsed();
     problems.iter().for_each(|problem| {
         println!("{}", problem.name());
---
+++ src/problem_02.rs
@@ -0,0 +1,162 @@
+use crate::problem::Problem;
+
+pub struct Problem02 {}
+
+#[derive(Debug, PartialEq, Eq)]
+enum RPSMove {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct RPSGame {
+    elf: RPSMove,
+    me: RPSMove,
+}
+
+impl RPSGame {
+    fn calculate_points(&self) -> i64 {
+        let mut total_points = match self.me {
+            RPSMove::Rock => 1,
+            RPSMove::Paper => 2,
+            RPSMove::Scissors => 3,
+        };
+
+        total_points += match (&self.elf, &self.me) {
+            (RPSMove::Rock, RPSMove::Paper)
+            | (RPSMove::Paper, RPSMove::Scissors)
+            | (RPSMove::Scissors, RPSMove::Rock) => 6,
+            (RPSMove::Rock, RPSMove::Scissors)
+            | (RPSMove::Paper, RPSMove::Rock)
+            | (RPSMove::Scissors, RPSMove::Paper) => 0,
+            _ => 3,
+        };
+
+        total_points
+    }
+}
+
+impl Problem02 {
+    pub fn new() -> Problem02 {
+        Problem02 {}
+    }
+
+    fn parse(&self, data: &str) -> Vec<RPSGame> {
+        let mut all_games = Vec::new();
+
+        for line in data.lines() {
+            let mut moves = line.split_ascii_whitespace().take(2);
+
+            all_games.push(RPSGame {
+                elf: match moves.next().unwrap() {
+                    "A" => RPSMove::Rock,
+                    "B" => RPSMove::Paper,
+                    _ => RPSMove::Scissors,
+                },
+                me: match moves.next().unwrap() {
+                    "X" => RPSMove::Rock,
+                    "Y" => RPSMove::Paper,
+                    _ => RPSMove::Scissors,
+                },
+            })
+        }
+
+        all_games
+    }
+
+    fn parse_part2(&self, data: &str) -> Vec<RPSGame> {
+        let mut all_games = Vec::new();
+
+        for line in data.lines() {
+            let mut moves = line.split_ascii_whitespace().take(2);
+
+            let elf_move = moves.next().unwrap();
+
+            all_games.push(RPSGame {
+                elf: match elf_move {
+                    "A" => RPSMove::Rock,
+                    "B" => RPSMove::Paper,
+                    _ => RPSMove::Scissors,
+                },
+                me: match (moves.next().unwrap(), elf_move) {
+                    ("X", "A") | ("Y", "C") | ("Z", "B") => RPSMove::Scissors,
+                    ("X", "B") | ("Y", "A") | ("Z", "C") => RPSMove::Rock,
+                    _ => RPSMove::Paper,
+                },
+            })
+        }
+
+        all_games
+    }
+
+    fn solve_actual(&self, rps_games: &[RPSGame]) -> i64 {
+        rps_games.iter().map(|g| g.calculate_points()).sum()
+    }
+}
+
+impl Problem for Problem02 {
+    fn name(&self) -> &str {
+        "Day 2: Rock Paper Scissors"
+    }
+
+    fn solve(&self) -> i64 {
+        let data = get_input!("./inputs/problem_02.txt");
+        let rps_games = self.parse(&data);
+        self.solve_actual(&rps_games)
+    }
+
+    fn solve_part2(&self) -> i64 {
+        let data = get_input!("./inputs/problem_02.txt");
+        let rps_games = self.parse_part2(&data);
+        self.solve_actual(&rps_games)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_actual_from_example() {
+        let data: Vec<RPSGame> = vec![
+            RPSGame {
+                elf: RPSMove::Rock,
+                me: RPSMove::Paper,
+            },
+            RPSGame {
+                elf: RPSMove::Paper,
+                me: RPSMove::Rock,
+            },
+            RPSGame {
+                elf: RPSMove::Scissors,
+                me: RPSMove::Scissors,
+            },
+        ];
+        let problem = Problem02::new();
+        assert_eq!(problem.solve_actual(&data), 15);
+    }
+
+    #[test]
+    fn test_parse_part2_from_example() {
+        let data = "A Y\nB X\nC Z".to_owned();
+        let problem = Problem02::new();
+        let parsed = problem.parse_part2(&data);
+        let expected_output: Vec<RPSGame> = vec![
+            RPSGame {
+                elf: RPSMove::Rock,
+                me: RPSMove::Rock,
+            },
+            RPSGame {
+                elf: RPSMove::Paper,
+                me: RPSMove::Rock,
+            },
+            RPSGame {
+                elf: RPSMove::Scissors,
+                me: RPSMove::Rock,
+            },
+        ];
+
+        assert_eq!(parsed, expected_output)
+    }
+}
